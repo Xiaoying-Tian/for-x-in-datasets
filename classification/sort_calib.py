7c7086a8fad30f34716302068eaa35395fd6596e
Xiaoying-Tian <catherinetxy1991@gmail.com>
2016-03-14 11:17:28 -0700

    more experimenting.

--- classification/sort_calib.py
+++ classification/sort_calib.py
@@ -12,9 +12,9 @@ from sklearn import cross_validation
 import matplotlib.pyplot as plt
 
 class PredError:
-    def __init__(self, X, y, classifier, linear_model=False, split=0.1):
+    def __init__(self, X, y, classifier, seed, linear_model=False, split=0.1):
         self.X_fit, self.X_test, self.y_fit, self.y_test = \
-            cross_validation.train_test_split(X, y, test_size=split)
+            cross_validation.train_test_split(X, y, test_size=split, random_state=seed)
         self.classifier = classifier
         self.linear_model = linear_model
 
@@ -22,26 +22,24 @@ class PredError:
         X, y = self.X_fit, self.y_fit
         self.classifier.fit(X, y)
         n = X.shape[0]
-        ones = np.where(y==1)[0]
-        zeros = np.where(y==0)[0]
-        FDR = sum((self.classifier.predict(X)==1) & (y==0)) * 1.0 / n 
-        FNR = sum((self.classifier.predict(X)==0) & (y==1)) * 1.0 / n
-        print "FDR:", FDR, " FNR:", FNR
         print log_loss(y, self.classifier.predict_proba(X)), "possibly optimistic"
 
-    def conditional(self, nbins=200):
+    def conditional(self, nbins=50):
         probs = self.classifier.predict_proba(self.X_test)[:,1].flatten()
-        score = self.classifier.decision_function(self.X_test).flatten() 
-        print score.max(), score.min()
-        bins = np.linspace(score.min(), score.max(), nbins)
+        #score = np.log(probs / (1-probs))
+        #score = self.classifier.decision_function(self.X_test).flatten() 
+        score = probs
+        #bins = np.linspace(score.min(), score.max(), nbins)
+        bins = np.percentile(score, list(np.linspace(0,100,nbins)))
         digitized = np.digitize(score, bins) - 1 
         bin_means = np.array([self.y_test[digitized == i].mean() for i in range(len(bins))])
         print np.isnan(bin_means).sum()
         cond_y = bin_means[digitized]
         self.calib = cross_entropy(cond_y, probs) - cross_entropy(cond_y, cond_y) 
         self.sort = cross_entropy(cond_y, cond_y)
-        print "actual prediction error: ", log_loss(self.y_test, probs)
+        print "actual prediction error: ", cross_entropy(self.y_test, probs)
         print "conditional error calculation: ", self.sort, self.calib
+        print "sorting + calibration: ", self.sort + self.calib
 
 def cross_entropy(p, q):
     if np.isnan(p).any() or np.isnan(q).any():
@@ -54,17 +52,23 @@ def main(split=0.1):
     y = X[X.columns[0]]
     del X[X.columns[0]]
 
+    seed = np.random.randint(0,10000)
     classifier = LogisticRegression()
-    pred = PredError(X, y, classifier, linear_model=True)
+    pred = PredError(X, y, classifier, seed, linear_model=True)
     pred.fit()
     pred.conditional()
     
     print "Now GradientBoosting Trees"
-    classifier = GradientBoostingClassifier(min_samples_leaf=10)
-    pred = PredError(X, y, classifier)
+    classifier = GradientBoostingClassifier()
+    pred = PredError(X, y, classifier, seed)
     pred.fit()
     pred.conditional()
-    plt.show()
+
+    print "Now RFs"
+    classifier = RandomForestClassifier(max_depth=10)
+    pred = PredError(X, y, classifier, seed)
+    pred.fit()
+    pred.conditional()
 
 if __name__ == "__main__":
     main()
